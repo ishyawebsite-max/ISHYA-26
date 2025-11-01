aa7df81f4846b4a1d41dfdb6f7595806f5a9f948
ishyawebsite-max <ishyawebsite@gmail.com>
2025-11-01 15:28:42 +0530

    Update script.js

--- ROOMS/script.js
+++ ROOMS/script.js
@@ -2,7 +2,7 @@
 
 // --- CONFIGURATION ---
 const GOOGLE_CLIENT_ID = '750824340469-nrqmioc1jgoe6rjnuaqjdu9mh0b4or2o.apps.googleusercontent.com'; // <-- IMPORTANT: Paste your Client ID here
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyjoW0O_mSuqX4nQU0wpnSGeQr0gV_tIq91uxfE8k7YDJvhtfF43m7rh7a0vqSOMmsqqQ/exec'; // <-- IMPORTANT: Paste your Web App URL here
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwsow-uSwtpLEODbtUlfSW908EsIPA-AijaH0kGZ2wZGGeCyvzqava4accf0TgKFKhxkw/exec'; // <-- IMPORTANT: Paste your Web App URL here
 
 
 let currentUser = null, rooms = [], selectedRoom = null, selectedDate = new Date(), selectedSlots = [], currentMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1);
