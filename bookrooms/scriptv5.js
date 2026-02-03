574b271702960209d948ab072fca8f9c75154020
ishyawebsite-max <ishyawebsite@gmail.com>
2026-02-03 14:15:50 +0530

    Update scriptv5.js

--- bookrooms/scriptv5.js
+++ bookrooms/scriptv5.js
@@ -1,7 +1,7 @@
 // script.js - VERSION 5.2 COMPLETE
 // --- CONFIGURATION ---
 const GOOGLE_CLIENT_ID = '750824340469-nrqmioc1jgoe6rjnuaqjdu9mh0b4or2o.apps.googleusercontent.com'; 
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw_aGbv_r4AAypGcm95djGKJqPBEgT41vWQjxBb0sbkqDJza5bW2lZw5kvBKmhkNB7rzA/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxi1jkCBDSZQ0JfYl9Tmhxo4C85Q29P7FaLyeJvvQVhWrHkRRg-3elwNP0cchmg5gg24Q/exec';
 
 
 let currentUser = null, rooms = [], selectedRoom = null, selectedDate = new Date(), selectedSlots = [], currentMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1);
